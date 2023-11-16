539d50fa47f43cc19d7a2b8bfbbc9345d6971b22
João Guilherme <jaaumgui@gmail.com>
2023-11-16 20:52:09 -0300

    Adicionado itens da IDE

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Jaaum/CLionProjects/BubbleSort
+# Install script for directory: C:/Users/Jaaum/CLionProjects/Sorting-Algorithms
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jaaum/CLionProjects/BubbleSort/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Jaaum/CLionProjects/Sorting-Algorithms/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
